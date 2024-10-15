7aa018d3b1a8837f4f5523c59a0901245cf733da
AndreSecco <andrefigueiro5@gmail.com>
2024-10-15 19:48:28 -0300

    Novos convidados para celula

--- app/Http/Controllers/Celulas/CelulasController.php
+++ app/Http/Controllers/Celulas/CelulasController.php
@@ -16,7 +16,7 @@ class CelulasController extends Controller
     {
         try {
             $select_celulas = Grupos::where('nr_seq_lider', $id_lider_celula)
-            ->get();
+                ->get();
 
             return response()->json($select_celulas, 200);
         } catch (Exception $error) {
@@ -28,6 +28,9 @@ class CelulasController extends Controller
         try {
             $select_celulas = DB::table('tab_grupo_membros as tgm')
                 ->join('tab_pessoas as tp', 'tgm.nr_seq_pessoa', '=', 'tp.nr_sequencial')
+                // ->with('nr_seq_pessoa', function ($query) {
+                //     $query->select('nr_sequencial', 'nr_seq_pessoa');
+                // })
                 ->where('nr_seq_grupo', $id_grupo)
                 ->get();
 
@@ -36,7 +39,8 @@ class CelulasController extends Controller
                 ->where('tc.nr_seq_grupo', $id_grupo)
                 ->get();
 
-            // $select_dados_celula->participantes = DB::table('')
+            // $select_dados_celula->participantes = DB::table('tab_celulas_presentes')
+            // ->where('nr_seq_grupo')
 
             return response()->json([
                 'membros_celula' => $select_celulas,
@@ -46,6 +50,7 @@ class CelulasController extends Controller
             return response()->json($error->getMessage(), 400);
         }
     }
+    
     public function finalizarCelula(Request $request)
     {
         try {
@@ -55,15 +60,45 @@ class CelulasController extends Controller
                 'ofertas_voluntarias' => $request->ofertas_voluntarias,
                 'nr_nivel' => 1,
                 'created_at' => date('Y-m-d m:i:s'),
-                'data_celula' => date('Y-m-d m:i:s')
+                'data_celula' => date('Y-m-d m:i:s'),
+                'nr_seq_pregou' => $request->quem_pregou,
             ]);
 
             foreach ($request->membros_presentes as $key => $value) {
                 $insert_membros_presentes = DB::table('tab_celula_presentes')->insert([
                     'nr_seq_celula' => $insert_celula,
                     'nr_seq_pessoa' => $value['nr_sequencial'],
-                    'nr_seq_tp_membro' => 1
+                    'nr_seq_tp_membro' => 4
                 ]);
+            }
+
+            if (is_array($request->array_convidados)) {
+                foreach ($request->array_convidados as $key => $value) {
+                    if (is_numeric($value['nome_convidado'])) {
+                        $insert_membros_presentes = DB::table('tab_celula_presentes')->insert([
+                            'nr_seq_celula' => $insert_celula,
+                            'nr_seq_pessoa' => $value['nome_convidado'],
+                            'nr_seq_tp_membro' => 4
+                        ]);
+                    } else {
+                        $insert_pessoa = DB::table('tab_pessoas')->insertGetId([
+                            'nome_pessoa' => $value['nome_convidado'],
+                            'sexo_pessoa' => 'I',
+                            'nr_nivel'    => 6,
+                            'id_parent'   => 1,
+                            'nr_seq_filial' => 1,
+                            'whatsapp' => $request->whatsapp,
+                            'st_ativo' => 'true'
+                        ]);
+
+                        $insert_membros_presentes = DB::table('tab_celula_presentes')->insert([
+                            'nr_seq_celula' => $insert_celula,
+                            'nr_seq_pessoa' => $insert_pessoa,
+                            'nr_seq_tp_membro' => 6,
+                            'quem_convidou' => $value['quem_convidou']
+                        ]);
+                    }
+                }
             }
 
             return response()->json($insert_celula, 200);
